51a408009d54d4b5afc436cb8bb402a07c848975
Subash Pudasaini <subashpudasaini1@gmail.com>
2023-04-28 23:59:06 +0545

    little changes

--- resources/views/pendingBlogs.blade.php
+++ resources/views/pendingBlogs.blade.php
@@ -19,16 +19,16 @@
             <div class="ml-auto mr-auto bg-gray-300 object-cover h-24 w-32  bg-cover bg-center"
                 style="background-image: url('{{ asset($post->imagePath) }}')">
             </div>
-            <p class="text-gray-700 mb-5 mt-4">{{ Str::limit($post->body, 100) }}</p>
+            <p class="text-gray-700 mb-4 mt-4">{{ Str::limit(html_entity_decode(strip_tags($post->body)), 100) }}</p>
             <div class="absolute bottom-8 right-1 text-gray-700">
                 <a href="{{ route('blog.show', $post) }}"> Read more...</a>
             </div>
         </div>
-        <div class="absolute bottom-1 right-1 ">
+        <div class="absolute bottom-1 right-1">
             <form action="{{ route('blog.approve', $post) }}" method="post" enctype="multipart/form-data">
                 @method('put')
                 @csrf
-                <input type="submit" value="Approve" class="px-2 border-rounded bg-green-500 text-white rounded"> 
+                <input type="submit" value="Approve" class="px-2 border-rounded bg-green-500 text-white rounded cursor-pointer"> 
             </form>
         </div>
     </div>
